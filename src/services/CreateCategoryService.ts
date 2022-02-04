95308de8d5fe4b5da96031787a896bf04be539ee
Andre Lima <amilandre@gmail.com>
2022-02-04 15:10:43 -0400

    crud node

---
+++ src/services/CreateCategoryService.ts
@@ -0,0 +1,25 @@
+
+import { getRepository } from "typeorm";
+import { Category } from "../entities/Category";
+
+type CategoryResquest = {
+    name: string;
+    description: string;
+}
+class CreateCategoryService {
+    async execute({ name, description}:CategoryResquest ): Promise<Category | Error> {
+        const repo = getRepository(Category);
+
+        //SELECT * FROM categories WHERE name = name LIMIT 1
+        if(await repo.findOne({ name })){
+            return new Error('Category already exists');
+        }
+
+        const category = repo.create({
+            name,
+            description
+        });
+        await repo.save(category);
+        return category;
+    }
+}
